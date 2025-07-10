46e082b71a6df1873dfe2723d76dfc5598a9506b
Yurii <yerikstar@gmail.com>
2025-07-10 23:17:48 +0300

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+
+const categories = document.querySelector('ul#categories');
+// console.log(categories);
+const quantityCategories = document.querySelectorAll('li.item');
+console.log(`Number of categories: ${quantityCategories.length}`);
+
