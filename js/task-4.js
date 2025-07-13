f74ada2e43f2f3c1945d932441bdf338595b5e62
Yurii <yerikstar@gmail.com>
2025-07-13 13:09:58 +0300

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const formElem = document.querySelector('.login-form');
+formElem.addEventListener('submit', e => {
+    e.preventDefault();
 
+    const email = formElem.elements.email.value.trim();
+    const password = formElem.elements.password.value.trim();
+
+    if (email === '' || password === '') {
+        alert('All form fields must be filled in');
+        return;
+    }
+
+    const userData = {
+        email,
+        password,
+    };
+    console.log(userData);
+    formElem.reset();
+});
